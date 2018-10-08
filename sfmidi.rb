8de0cb6c8fe260275a36883bc02a9ba25f6c3579
Giovanni Merlino <gmerlino@unime.it>
2018-10-08 06:34:50 +0200

    fixes for audit strict compliance (almost)

--- sfmidi.rb
+++ sfmidi.rb
@@ -1,18 +1,21 @@
-require "formula"
-
 class Sfmidi < Formula
-  homepage ""
-  url "http://www.zorexxlkl.com/files/downloads/sfMidi-1.1.0-CMake.zip"
-  sha256 "2ba1de398e51c81e3178f8f075bda0119596a0fb227ff62d6633d3928a406522"
+  desc "SfMidi"
+  homepage "https://github.com/JonnyPtn/sfMidi.git"
+  head "https://github.com/JonnyPtn/sfMidi.git"
 
   depends_on "cmake" => :build
+  depends_on "fluid-synth" => :build
   depends_on "sfml" => :build
-  depends_on "fluidsynth" => :build
 
   def install
     # Include Error.h among installed header files
-    system '/usr/local/bin/gsed -i -e "s/.*Loader.h.*/\${INCSFMIDI}\/Loader.h\\n\${INCSFMIDI}\/Error.h/" CMakeLists.txt'
-    system "cmake", ".", *std_cmake_args
+    # system '/usr/local/bin/gsed -i -e "s/.*Loader.h.*/\${INCSFMIDI}\/Loader.h\\n\${INCSFMIDI}\/Error.h/" CMakeLists.txt'
+    system "cmake", ".", "-DCMAKE_MODULE_PATH=#{Formula["sfml"].share}/SFML/cmake/Modules", "-DCMAKE_PREFIX_PATH=#{Formula["sfml"].share}/SFML/cmake/Modules", *std_cmake_args
     system "make", "install"
+  end
+
+  # to be fixed
+  test do
+    system "#{bin}/sfmidi", "--help"
   end
 end
